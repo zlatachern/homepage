2009a06f33a8f5fe862dd028493b0ec7c8bd6f39
zlata <chernigovazlata2017@gmail.com>
2023-06-18 17:03:58 +0300

    Update project

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+
+// Bootstrap code for accordion
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+// Button--popup opened & closed
 const button = document.querySelector(".button--popup");
 const popup = document.getElementById("popup");
 
